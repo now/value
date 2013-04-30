9ebf9eb20a70a3af1e5ff880aadfa6ad1ddf6be3
Nikolai Weibull <now@bitwi.se>
2013-04-30 13:06:05 +0200

    Value 1.1.2

--- lib/value-1.0/version.rb
+++ lib/value-1.0/version.rb
@@ -3,10 +3,10 @@
 require 'inventory-1.0'
 
 module Value
-  Version = Inventory.new(1, 1, 1){
+  Version = Inventory.new(1, 1, 2){
     def dependencies
       super + Inventory::Dependencies.new{
-        development 'inventory-rake', 1, 3, 0
+        development 'inventory-rake', 1, 4, 0
         development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
         development 'yard', 0, 8, 0
