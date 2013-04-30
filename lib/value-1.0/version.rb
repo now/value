00d22c42af194d166270e219460f978f7db301e9
Nikolai Weibull <now@bitwi.se>
2013-04-30 14:37:16 +0200

    Add Inventory-Rake-Task-YARD dependency

--- lib/value-1.0/version.rb
+++ lib/value-1.0/version.rb
@@ -7,6 +7,7 @@ module Value
     def dependencies
       super + Inventory::Dependencies.new{
         development 'inventory-rake', 1, 4, 0
+        development 'inventory-rake-tasks-yard', 1, 3, 0
         development 'lookout', 3, 0, 0
         development 'lookout-rake', 3, 0, 0
         development 'yard', 0, 8, 0
