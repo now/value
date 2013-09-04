8bd3c5e0b96c558983d44ebbe73dd1d391c35049
Nikolai Weibull <now@bitwi.se>
2013-09-04 14:56:00 +0200

    Update dependencies

--- Rakefile
+++ Rakefile
@@ -4,11 +4,7 @@ require 'inventory-rake-1.0'
 
 load File.expand_path('../lib/value-1.0/version.rb', __FILE__)
 
-Inventory::Rake::Tasks.define Value::Version, :gem => proc{ |_, s|
-  s.author = 'Nikolai Weibull'
-  s.email = 'now@bitwi.se'
-  s.homepage = 'https://github.com/now/value'
-}
+Inventory::Rake::Tasks.define Value::Version
 
 Inventory::Rake::Tasks.unless_installing_dependencies do
   require 'lookout-rake-3.0'
--- lib/value-1.0/version.rb
+++ lib/value-1.0/version.rb
@@ -3,15 +3,27 @@
 require 'inventory-1.0'
 
 module Value
-  Version = Inventory.new(1, 1, 2){
+  Version = Inventory.new(1, 1, 3){
+    authors{
+      author 'Nikolai Weibull', 'now@disu.se'
+    }
+
+    homepage 'http://disu.se/software/value'
+
+    licenses{
+      license 'LGPLv3+',
+              'GNU Lesser General Public License, version 3 or later',
+              'http://www.gnu.org/licenses/'
+    }
+
     def dependencies
       super + Inventory::Dependencies.new{
-        development 'inventory-rake', 1, 4, 0
-        development 'inventory-rake-tasks-yard', 1, 3, 0
+        development 'inventory-rake', 1, 6, 0
+        development 'inventory-rake-tasks-yard', 1, 4, 0
         development 'lookout', 3, 0, 0
-        development 'lookout-rake', 3, 0, 0
+        development 'lookout-rake', 3, 1, 0
         development 'yard', 0, 8, 0
-        development 'yard-heuristics', 1, 1, 0
+        development 'yard-heuristics', 1, 2, 0
       }
     end
 
